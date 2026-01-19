7f4282b32c623e32ed563401f613688f6f114b8a
soniagkalliath <soniageorge111@gmail.com>
2026-01-19 13:06:20 +0530

    Server created

---
+++ config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const connectionString = process.env.DB_URL
+
+mongoose.connect(connectionString).then(res=>{
+    console.log("Database connection successfull...");    
+}).catch(error=>{
+    console.log("Database connection failed!!");
+    console.log(error);    
+})
---
+++ controllers/recipeController.js
@@ -0,0 +1,13 @@
+const recipes = require('../models/recipeModel')
+
+//get  all recipes
+exports.getAllRecipesController = async (req,res)=>{
+    console.log('Inside getAllRecipesController');
+    try{
+        const allRecipes = await recipes.find()
+        res.status(200).json(allRecipes)
+    }catch(error){
+        console.log(error);
+        res.status(500).json(error)
+    }
+}
