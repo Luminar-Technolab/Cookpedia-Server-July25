3ba85c23ec35029ad903558e444f7e5b6662dcb5
soniagkalliath <soniageorge111@gmail.com>
2026-01-21 15:32:10 +0530

    ADD: register api added

---
+++ controllers/userController.js
@@ -0,0 +1,24 @@
+const users = require('../models/userModel')
+const bcrypt = require('bcrypt')
+
+//register user
+
+exports.registerController = async (req,res)=>{
+    console.log("inside registerController");
+    const {username,email,password} = req.body
+    try{
+        const existingUser = await users.findOne({email})
+        if(existingUser){
+            res.status(409).json("User already exist!!! Please Login")
+        }else{
+            const encryptedPassword = await bcrypt.hash(password,10)
+            const newUser = await users.create({
+                username,email,password:encryptedPassword
+            })
+            res.status(200).json(newUser)
+        }
+    }catch(error){
+        console.log(error);
+        res.status(500).json(error)
+    }
+}
--- routes/routing.js
+++ routes/routing.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const recipeController = require('../controllers/recipeController')
-
+const userController = require('../controllers/userController')
 
 const router = new express.Router()
 
 //get all recipes
 router.get('/recipes',recipeController.getAllRecipesController)
+//register
+router.post('/register',userController.registerController)
 
 module.exports = router
