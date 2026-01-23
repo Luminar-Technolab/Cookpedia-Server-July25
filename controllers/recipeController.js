d7a80f9614bed8d4a9f1b1df439317b3a8b19e1d
soniagkalliath <soniageorge111@gmail.com>
2026-01-23 13:42:46 +0530

    view recipe

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -11,3 +11,30 @@ exports.getAllRecipesController = async (req,res)=>{
         res.status(500).json(error)
     }
 }
+
+//view recipe 
+exports.viewRecipeController = async (req,res)=>{
+    console.log('Inside viewRecipeController');
+    const {id} = req.params
+    try{
+        const viewRecipe = await recipes.findById({_id:id})
+        res.status(200).json(viewRecipe)
+    }catch(error){
+        console.log(error);
+        res.status(500).json(error)
+    }
+}
+
+//related recipe 
+exports.relatedRecipesController = async (req,res)=>{
+    console.log('Inside relatedRecipesController');
+    const cuisine = req.query.cuisine
+    
+    try{
+        const allRecipes = await recipes.find({cuisine})
+        res.status(200).json(allRecipes)
+    }catch(error){
+        console.log(error);
+        res.status(500).json(error)
+    }
+}
--- routes/routing.js
+++ routes/routing.js
@@ -10,5 +10,9 @@ router.get('/recipes',recipeController.getAllRecipesController)
 router.post('/register',userController.registerController)
 //login
 router.post('/login',userController.loginController)
+//view  recipe
+router.get('/recipes/:id',recipeController.viewRecipeController)
+//get related  recipe
+router.get('/related-recipes',recipeController.relatedRecipesController)
 
 module.exports = router
